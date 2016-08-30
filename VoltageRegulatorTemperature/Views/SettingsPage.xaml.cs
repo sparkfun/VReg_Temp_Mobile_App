e1d6a865fe8c6016be6e463d51c4799b255d6fd1
Brent Wilkins <brent@SparkFun.com>
2016-08-30 16:16:03 -0600

    Added untracked file

---
+++ VoltageRegulatorTemperature/Views/SettingsPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace VoltageRegulatorTemperature.Views
+{
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
